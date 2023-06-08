7ce317e534d7bdc222f1b5d21e0533486914698b
Gmanlove <emmagmanc@gmail.com>
2023-06-08 18:32:07 +0100

    validations and model specs

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '3.1.2'
 
 gem 'pg', '~> 1.1'
 
+gem 'rspec-rails', '~> 4.0'
+
 gem 'rubocop', '>= 1.0', '< 2.0'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.5'
---
+++ spec/comment_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
+  let(:post) { user.posts.create(title: 'Post 1', text: 'This is post 1', comments_counter: 0, likes_counters: 0) }
+  subject { Comment.new(text: 'Text', author_id: user.id, post_id: post.id) }
+
+  before { subject.save }
+
+  it 'validates presence of text' do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'validates presence of author_id' do
+    subject.author_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'validates presence of post_id' do
+    subject.post_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  describe '#update_post_comments_counter' do
+    it 'increments the comments counter' do
+      expect { subject.update_post_comments_counter }.to change { subject.post.comments_counter }.by(1)
+    end
+  end
+end
