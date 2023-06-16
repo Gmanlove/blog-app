a792b9dc0c102fa348d2e38c6503ffa7ebfff125
Gmanlove <emmagmanc@gmail.com>
2023-06-16 10:26:03 +0100

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,52 @@
 class PostsController < ApplicationController
+  layout 'standard'
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+    @current_user = current_user
   end
 
   def show
     @post = Post.find(params[:id])
+    @current_user = current_user
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build
+  end
+
+  def like
+    @post = Post.find(params[:id])
+    @like = Like.new(author_id: params[:user_id], post_id: @post.id)
+
+    if @like.save
+      @post.likes_counters += 1 # Increment the likes count by 1
+      @post.save
+      redirect_to user_post_path(params[:user_id], @post), notice: 'Like added successfully'
+    else
+      redirect_to user_post_path(params[:user_id], @post), alert: 'Failed to add like'
+    end
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
+    @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counters = 0
+
+    if @post.save
+      redirect_to user_posts_path(@user), notice: 'Post created successfully'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create, :new] do
+      resources :comments, only: [:create, :new]
+      post 'like', on: :member
+    end
+
  end
 
   root 'users#index'
