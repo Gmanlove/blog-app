3f74eafe7858e50fbe3aa1d266f67665282df9cc
belalgamal46 <belalgamal00@gmail.com>
2023-06-19 17:14:13 +0300

    Add post show page integration test

---
+++ spec/features/post_show_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.describe 'Post Show Page', type: :feature do
+  before do
+    # Set up any necessary data or visit relevant pages
+  end
+
+  it 'displays the post title' do
+    # Test code goes here
+  end
+
+  it 'displays the author of the post' do
+    # Test code goes here
+  end
+
+  it 'displays the number of comments the post has' do
+    # Test code goes here
+  end
+
+  it 'displays the number of likes the post has' do
+    # Test code goes here
+  end
+
+  it 'displays the post body' do
+    # Test code goes here
+  end
+
+  it 'displays the username of each commentor' do
+    # Test code goes here
+  end
+
+  it 'displays the comment each commentor left' do
+    # Test code goes here
+  end
+end
