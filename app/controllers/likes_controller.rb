348fbf2080d86893cc07c5d3a694696614f312ce
Gmanlove <emmagmanc@gmail.com>
2023-06-24 00:59:56 +0100

    Update the reviwer requirement

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,7 +4,6 @@ class LikesController < ApplicationController
     @user = current_user
 
     if @post.likes.create(author: @user)
-      @post.increment!(:likes_counter)
       redirect_to user_post_path(user_id: @post.author_id, id: @post.id)
     else
       render :new, status: :unprocessable_entity
